fc920c8cbde785e228f48d09238d082384d38bc5
rachel145 <146837213+rachel145@users.noreply.github.com>
2023-11-07 10:22:53 +0100

    nästan färdig

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+        var LoginForm = document.getElementById ("LoginForm");
+        var RegForm = document. getElementById ("RegForm");
+        var Indicator = document.getElementById ("Indicator");
+
+       /*function register ()[
+            
+        RegForm.style.transform = "translateX (0px)";
+        LoginForm.style.transform = "translateX (0px)";
+        ]
+
+        function login ()[
+            
+            RegForm.style.transform = "translateX (300px)";
+            LoginForm.style.transform = "translateX (300px)";
+            ]
+*/
+
+login()
+
+function login() {
+
+    LoginForm.hidden = false 
+RegForm.hidden = true 
+}
+
+function register () {
+    LoginForm.hidden = true
+    RegForm.hidden = false 
+} 
+
+
+    
+
+
+
+    
